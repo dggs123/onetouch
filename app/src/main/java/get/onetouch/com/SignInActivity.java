a3b675f56d7d1be527c3117f6372ce78ecc978a1
Gulzar Ahmed <gulzarhome54@gmail.com>
2016-08-29 17:27:00 +0530

    TapActivity Created

--- app/src/main/java/get/onetouch/com/SignInActivity.java
+++ app/src/main/java/get/onetouch/com/SignInActivity.java
@@ -1,15 +1,17 @@
 package get.onetouch.com;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 /**
  * Created by Gulzar on 28-08-2016.
  */
-public class SignInActivity extends AppCompatActivity{
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
     TextView mAppTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,18 @@ public class SignInActivity extends AppCompatActivity{
         Typeface Roboto = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
         mAppTitle.setText("OneTouch");
         mAppTitle.setTypeface(Roboto);
+        findViewById(R.id.btn_login).setOnClickListener(this);
 
 
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.btn_login:
+                startActivity(new Intent(SignInActivity.this,TapButtonActivity.class));
+                break;
+        }
     }
 }
